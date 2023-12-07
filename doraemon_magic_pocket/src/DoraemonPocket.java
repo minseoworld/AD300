123e65a85cc6430b36c8b7285380685dc5c64897
minseoworld <147306227+minseoworld@users.noreply.github.com>
2023-12-07 00:53:15 -0800

    Add files via upload

--- doraemon_magic_pocket/src/DoraemonPocket.java
+++ doraemon_magic_pocket/src/DoraemonPocket.java
@@ -2,20 +2,28 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+//DoraemonPocket; Space Complexity: O(n)
+//The pocket needs space proportional to the number of items it contains.
 public class DoraemonPocket {
     private List<String> pocketItems = new ArrayList();
 
     public DoraemonPocket() {
     }
 
+    //addItem; Time Complexity: O(1)
+    //Adding items is usually fast, taking a short amount of time.
     public void addItem(String itemName) {
         this.pocketItems.add(itemName);
     }
 
+    //removeItem; Time Complexity: O(n)
+    //Removing items might take longer, especially if many items are in the pocket.
     public void removeItem(String itemName) {
         this.pocketItems.remove(itemName);
     }
 
+    //printItmes; Time Complexity: O(n)
+    //Printing all items also takes time, and it depends on the number of items.
     public void printItems() {
         Iterator var1 = this.pocketItems.iterator();
 
@@ -26,6 +34,8 @@ public class DoraemonPocket {
 
     }
 
+    //getNumItems; Time Complexity: O(1)
+    //Checking the number of items is quick and doesn't take much time.
     public int getNumItems() {
         return this.pocketItems.size();
     }
