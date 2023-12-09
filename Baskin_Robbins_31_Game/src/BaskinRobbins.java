1d75679c86ede0d1b8daed69a49d2b5056f61fa2
minseoworld <147306227+minseoworld@users.noreply.github.com>
2023-12-09 14:57:54 -0800

    Add files via upload

--- Baskin_Robbins_31_Game/src/BaskinRobbins.java
+++ Baskin_Robbins_31_Game/src/BaskinRobbins.java
@@ -1,35 +1,41 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class BaskinRobbins{
+public class BaskinRobbins {
 
     private static Map<Integer, Integer> memo = new HashMap<>();
 
     public static void main(String[] args) {
         int n = 31;
         int ways = baskin(n);
+        System.out.println("Number of distinct ways the game can be played: " + ways);
     }
 
     public static int baskin(int n) {
+        // Base case: if n is less than or equal to 0, there are no ways to play
         if (n <= 0) {
             return 0;
         }
-
+        // Base case: if n is 1, there are two ways to play (say "1" or "1, 2")
         if (n == 1) {
-            return 1;
+            return 2;
         }
 
+        // Check if the result for the current n is already memoized
         if (memo.containsKey(n)) {
             return memo.get(n);
         }
 
+        // Recursive case: count ways for saying "1" and "1, 2" and sum them up
         int ways = baskin(n - 1) + baskin(n - 2);
 
+        // Memoize the result for the current n
         memo.put(n, ways);
 
         return ways;
     }
 }
+
 
 /*
 * Time Complexity
