f1cf988b3931167912cc0e7f67ff8e9428558b22
minseoworld <147306227+minseoworld@users.noreply.github.com>
2023-12-14 18:50:27 -0800

    Update Main.java

--- Project_1/src/Main.java
+++ Project_1/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of SongManager
         SongManager songManager = new SongManager();
 
-        // Create an instance of SongViewer, passing the SongManager as a parameter
         SongViewer songViewer = new SongViewer(songManager);
     }
 }
